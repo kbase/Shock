3d50bee9bb307f0129ec70030f2c1e04ee67ea01
Steve Chan <sychan168@gmail.com>
2017-06-20 00:00:43 -0700

    Use proper env var from travis env

--- build_shock_image.sh
+++ build_shock_image.sh
@@ -1,6 +1,6 @@
 	export BRANCH=${TRAVIS_BRANCH:-`git symbolic-ref --short HEAD`}
 	export DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
-	export COMMIT=${COMMIT:-`git rev-parse --short HEAD`}
+	export COMMIT=${TRAVIS_COMMIT:-`git rev-parse --short HEAD`}
 	docker run -v `pwd`/deployment:/kb/deployment -v `pwd`/build_shock_binary.sh:/tmp/build_shock_binary.sh -e COMMIT=$COMMIT kbase/kb_go /tmp/build_shock_binary.sh
 	docker build --build-arg BUILD_DATE=$DATE \
      		     --build-arg VCS_REF=$COMMIT \
