869b20bee09465fc8f84b7b4db919948c6f57a8d
Steve Chan <sychan168@gmail.com>
2017-06-19 20:33:09 -0700

    Another missing docker prefix

--- build_shock_image.sh
+++ build_shock_image.sh
@@ -1,7 +1,7 @@
 	export BRANCH=`git symbolic-ref --short HEAD`
 	export DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
 	export COMMIT=`git rev-parse --short HEAD`
-	docker run -v `pwd`/deployment:/kb/deployment -v `pwd`/build_shock_binary.sh:/tmp/build_shock_binary.sh -e COMMIT=$COMMIT kb_go /tmp/build_shock_binary.sh
+	docker run -v `pwd`/deployment:/kb/deployment -v `pwd`/build_shock_binary.sh:/tmp/build_shock_binary.sh -e COMMIT=$COMMIT kbase/kb_go /tmp/build_shock_binary.sh
 	docker build --build-arg BUILD_DATE=$DATE \
      		     --build-arg VCS_REF=$COMMIT \
 				 --build-arg BRANCH=$BRANCH \
