a235306f5026cb1c3ba00f7ca9d3183faf93247f
Steve Chan <sychan168@gmail.com>
2017-06-21 11:37:47 -0700

    Fixed typo in entrypoint and added gitignore for generated config

--- deployment/bin/entrypoint.sh
+++ deployment/bin/entrypoint.sh
@@ -4,5 +4,6 @@
 # a jinja2 config template and writing out the config before starting the service
 #
 
-/usr/local/bin/j2 /kb/deployment/conf/.tenplates/shock-server.cfg.j2 > /kb/deployment/conf/shock-server.cfg && \
-echo /kb/deployment/bin/shock-server --conf /kb/deployment/conf/shock-server.cfg
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+/usr/local/bin/j2 $DIR/../conf/.templates/shock-server.cfg.j2 > $DIR/../conf/shock-server.cfg && \
+echo $DIR/shock-server --conf $DIR/../conf/shock-server.cfg
